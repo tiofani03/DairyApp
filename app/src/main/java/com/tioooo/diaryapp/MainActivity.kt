4eded92e8ba0c1ca284e05e53f50bfc4e9c6db58
Tio <tiofani03@gmail.com>
2023-06-03 10:40:55 +0700

    feat: implement splashscreen

--- app/src/main/java/com/tioooo/diaryapp/MainActivity.kt
+++ app/src/main/java/com/tioooo/diaryapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.tioooo.diaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tioooo.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryAppTheme {
 
