dfa83dac5c30c09a28eb3bb75f84dee39c24fa7e
Tio <tiofani03@gmail.com>
2023-06-12 16:51:18 +0700

    add app id

---
+++ app/src/main/java/com/tioooo/diaryapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.tioooo.diaryapp.utils
+
+import com.tioooo.diaryapp.BuildConfig
+
+object Constants {
+    const val APP_ID = BuildConfig.APP_ID
+    const val CLIENT_ID = BuildConfig.CLIENT_ID
+}
